8923762af87d841e0482ccb97d5b4bedc5ad6e0b
Rachwan <rachwan.harb2023@gmail.com>
2024-02-14 22:20:05 +0200

    Add UseContext

---
+++ src/UseContext/UserContext.js
@@ -0,0 +1,52 @@
+import { createContext, useState, useEffect } from "react";
+import axios from "axios";
+
+export const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState();
+  const [checkUser, setCheckUser] = useState(true);
+  const [userUpdated, setUserUpdated] = useState(false);
+
+  useEffect(() => {
+    if (!user || userUpdated) {
+      fetchUserData();
+    }
+  }, [user, userUpdated]);
+
+  const fetchUserData = async () => {
+    try {
+      setCheckUser(true);
+
+      const response = await axios.get(
+        `${process.env.NEXT_PUBLIC_BACK_END}/logged-in-user`
+      );
+      setUser(response.data.user);
+      console.log("Fetched user data:", response.data.user);
+      setUserUpdated(false);
+    } catch (err) {
+      setUser(null);
+    } finally {
+      setCheckUser(false);
+    }
+  };
+
+  const logout = async () => {
+    await axios.post(`${process.env.REACT_APP_PATH}/logout`);
+    setUser(null);
+  };
+
+  return (
+    <UserContext.Provider
+      value={{
+        user,
+        setUser,
+        logout,
+        fetchUserData,
+        checkUser,
+        setUserUpdated,
+      }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
